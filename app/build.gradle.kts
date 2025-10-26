0ec897cd43e8e22cd7d4e29bae0667e0b6b7f18a
hemanthcodecrit50 <hemanthcodecrit50@gmail.com>
2025-10-26 15:57:22 +0530

    firebaseAuthSetup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -64,4 +65,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-auth")
 }
